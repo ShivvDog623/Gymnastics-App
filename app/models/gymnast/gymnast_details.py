f229212ed9b92a0f48270d6ab615b8b593466f69
Shivam Patel <144988617+ShivvDog623@users.noreply.github.com>
2025-12-19 02:48:19 -0500

    refactored gymnast

--- app/models/gymnast/gymnast_details.py
+++ app/models/gymnast/gymnast_details.py
@@ -8,47 +8,54 @@ from typing import Optional
 class Gymnast(Base):
     __tablename__ = "gymnast"
 
-    id = Column(Integer, primary_key=True, index=True)
+    gymnast_id = Column(Integer, primary_key=True, index=True)
     gymnast_number = Column(Integer, unique=True, index=True)
     first_name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=False)
     birthday = Column(Date, nullable=False)
-    level = Column(String, nullable=False)
-    usag_number = Column(Integer, unique=True, index=True)
-    session = Column(String, nullable=False)
     events = Column(String, nullable=False)
+    gym_id = Column(Integer, nullable=False)
+    level = Column(String, nullable=True)
+    age = Column(Integer, nullable=False)
+    age_division = Column(String, nullable=True)
+    usag_number = Column(Integer, unique=True, nullable=False)
+    session_id = Column(Integer, nullable=False)
+    team_id = Column(Integer, nullable=True)
+
 
 # Pydantic Models
-class GymnastCreate(BaseModel):
+class GymnastBase(BaseModel):
     gymnast_number: int
     first_name: str
     last_name: str
     birthday: date
-    level: str
+    events: str
+    gym_id: int
+    level: Optional[str] = None
+    age: int
+    age_division: Optional[str] = None
     usag_number: int
-    session: str
-    events: str 
+    session_id: int
+    team_id: Optional[int] = None
 
-class GymnastResponse(BaseModel):
-    id: int
-    gymnast_number: int
-    first_name: str
-    last_name: str
-    birthday: date
-    level: str
-    usag_number: int
-    session: str
-    events: str 
-    
+class GymnastCreate(GymnastBase):
+    pass
+
+class GymnastResponse(GymnastBase):
+    gymnast_id: int    
     class Config: 
         from_attributes = True
 
 class GymnastUpdate(BaseModel):
-    gymnast_number: int | None = None
-    first_name: str | None = None
-    last_name: str | None = None
-    birthday: date | None = None
-    level: str | None = None
-    usag_number: int | None = None
-    session: str | None = None
-    events: str | None = None 
+    gymnast_number: Optional[int] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    birthday: Optional[date] = None
+    events: Optional[str] = None
+    gym_id: Optional[int] = None
+    level: Optional[str] = None
+    age: Optional[int] = None
+    age_division: Optional[str] = None
+    usag_number: Optional[int] = None
+    session_id: Optional[int] = None
+    team_id: Optional[int] = None
